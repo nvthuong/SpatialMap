9e3de70df8b5654101dd2276fb1fa0276c729700
Thuong Nguyen <nvthuong@Thuongs-Mac-mini.local>
2017-08-01 15:09:25 +0700

    db helper

---
+++ src/Services/Database/DbHelper.js
@@ -0,0 +1,22 @@
+import Point from '../Models/Point';
+import Line from '../Models/Line';
+import Polygon from '../Models/Polygon';
+
+class DbHelper {
+    modelSchema = [Point, Line, Polygon];
+
+    getInstance = () =>{
+        // TODO
+    }
+
+    create = () =>{
+        // TODO
+    }
+    read = () =>{
+        // TODO
+    }
+
+    write = () =>{
+        // TODO
+    }
+}
